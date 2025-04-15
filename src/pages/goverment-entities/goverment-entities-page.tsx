1160691a49561c5d731933e473acc3706e3f7057
Wilkin Vásquez <wilkinvasquez@gmail.com>
2025-04-15 18:26:58 -0400

    Fixed bug in pagination

--- src/pages/goverment-entities/goverment-entities-page.tsx
+++ src/pages/goverment-entities/goverment-entities-page.tsx
@@ -11,35 +11,40 @@ import Button from '../../components/button/button';
 
 const GovermentEntitiesPage = () => {
     const [tableRows, setTableRows] = useState<Array<GovermentEntity>>([]);
-    const [totalItems, setTotalItems] = useState<number>(2);
+    const [totalItems, setTotalItems] = useState<number>();
     const [pageNumber, setPageNumber] = useState<number>(1);
-    const [pageSize, setPageSize] = useState<number>(0);
-    const [searchString, setSearchString] = useState<string | null>('');
+    const [pageSize, setPageSize] = useState<number>(2);
+    const [filter, setFilter] = useState<string | null>('');
+    const [activeFilter, setActiveFilter] = useState<string | null>('');
 
     const getData = useCallback(() => {
         GovermentEntityService
-            .getAllPaged(pageNumber, pageSize, searchString)
+            .getAllPaged(pageNumber, pageSize, activeFilter)
             .then((result: Result<PaginationResponse<GovermentEntity>>) => {
                 setTableRows(result.data?.items ?? []);
                 setTotalItems(result.data?.total ?? 0);
             });
-    }, [pageNumber, pageSize, searchString]);
-
-    // const search = useCallback(() => {
-    //     getData(pageNumber, pageSize);
-    // }, [getData, pageNumber, pageSize]);
+    }, [pageNumber, pageSize, activeFilter]);
 
     useEffect(() => {
         getData();
-    }, [getData])
+    }, [getData]);
+
+    useEffect(() => {
+        setPageNumber(1);
+    }, [activeFilter]);
+
+    const onSearch = () => {
+        setActiveFilter(filter);
+    }
 
     return (
         <div className='goverment-entities-page-container'>
             <Input
                 title={'Buscar'}
-                value={searchString}
+                value={filter}
                 type={'text'}
-                callback={setSearchString}
+                callback={setFilter}
             />
 
             <Button
@@ -47,7 +52,7 @@ const GovermentEntitiesPage = () => {
                 buttonClassName={''}
                 showLoading={false}
                 disabled={false}
-                callback={getData}
+                callback={onSearch}
             />
 
             <GovermentEntitiesTable
